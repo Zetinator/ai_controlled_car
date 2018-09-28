a399dd53209d170297f56601b8b130c61d4ec7cb
Zetinator <erick_zetina@hotmail.com>
2018-09-28 14:56:01 -0500

    utils... updated

--- src/ai_car_control/scripts/utils.py
+++ src/ai_car_control/scripts/utils.py
@@ -19,33 +19,10 @@ class DataLoader(object):
     def load_labels (self):
         self.labels = np.genfromtxt(os.path.join(self.dataset_path, 'labels.txt'), delimiter=',')
         self.labels = self.labels.reshape(np.append(self.labels.shape,1))
-
-    def get_labels (self):
-        return (self.labels/self.labels[:,0,:].max())[:,0,:]
+        return (self.labels/180)[:,0,:]
 
     def load_images(self):
-        image_paths = sorted(glob.glob(os.path.join(self.dataset_path, '*.png')))
-        images = [cv2.imread(img,0) for img in image_paths]
-        self.images = np.array(images)
-        self.images = self.images.reshape(np.append(self.images.shape,1))
+        self.images = np.load(os.path.join(self.dataset_path, 'compressed_data.npz'))['images']
+        return self.images
 
-    def get_training (self):
-        training = self.images[:int(np.ceil(self.images.shape[0]*.7))]
 
-        return self.normalize(training)
-
-    def get_test (self):
-        test = self.images[int(np.ceil(self.images.shape[0]*.7)):]
-
-        return self.normalize(test)
-
-    def get_all (self):
-        test = self.normalize(self.images[:])
-
-        return self.normalize(test)
-
-    def normalize (self, data):
-        # normalize and reshape data between 0 and 1
-        data = (data/data.max()).astype('uint8')
-
-        return data
