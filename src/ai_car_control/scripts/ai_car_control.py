c1d80fa156372c9c0c10706e3648b7681b7983ba
Zetinator <erick_zetina@hotmail.com>
2018-09-30 18:10:01 -0500

    ultimate --> v1, v2 weights added

--- src/ai_car_control/scripts/ai_car_control.py
+++ src/ai_car_control/scripts/ai_car_control.py
@@ -53,7 +53,7 @@ class CarController:
         self.settings.ydim = 40
         self.settings.xdim = 80
         self.settings.channels = 1
-        self.settings.model_weights = "/DATA/Documents/ai_controlled_car/src/ai_car_control/weights/ultimate.h5"
+        self.settings.model_weights = "/DATA/Documents/ai_controlled_car/src/ai_car_control/weights/v2.h5"
 
         # load model
         print("WARMING UP...")
